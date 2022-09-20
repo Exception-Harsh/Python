13b8107a3dc347721f0017e406fbe86a1d8e456d
Harsh Singh <111991803+Exception-Harsh@users.noreply.github.com>
2022-09-20 13:55:17 +0530

    Add files via upload

---
+++ Practicals/Exp7.py
@@ -0,0 +1,42 @@
+class emp:
+	def __init__(self, id1, name1):
+		self.id = id1
+		self.name = name1
+		
+	def display(self):
+		print(self.id)
+		print(self.name)
+		
+	def setvalue(self, id1, name1):
+		self.id = id1
+		self.name = name1
+lst1 = list()
+n = int(input("Enter Number of Employee: "))
+
+for i in range(n):
+	z = int(input("Enter Employee ID: "))
+	a = input("Enter Name of Employee: ")
+	e1 = emp(z, a)
+	lst1.append(e1)
+
+while 1:
+	print("1: Print \t2: Change ID  \t3: Change Name \t5: exit ")
+	choice = int(input("Enter Choice: "))
+	
+	if choice == 1:
+		for i in list1:
+			print("ID: ", i.id, "\nName: ", i.name)
+	elif choice == 2:
+		q = input("Enter Name of the employee to be changed: ")
+		nid = int(input("Enter new ID: "))
+		for d in lst1:
+			if q == d.name:
+				d.setvalue(nid, q)
+		print("After Change: ")
+		for i in lst1:
+			print("ID: ", i.id, "Name: ", i.name)
+		
+	elif choice == 4:
+		exit()
+			
+	
