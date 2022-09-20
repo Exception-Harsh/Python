0984fb77917a239843abc47b0302ba105143872a
Exception-Harsh <harsh180403singh@gmail.com>
2022-09-20 20:47:11 +0530

    commit

--- Practicals/Exp7.py
+++ Practicals/Exp7.py
@@ -28,6 +28,7 @@ while 1:
     if choice == 1:
         for i in lst1:
             print("ID: ", i.id, "\nName: ", i.name)
+
     elif choice == 2:
         q = input("Enter Name of the employee to be changed: ")
         nid = int(input("Enter new ID: "))
@@ -37,6 +38,7 @@ while 1:
         print("After Change: ")
         for i in lst1:
             print("ID: ", i.id, "Name: ", i.name)
+
     elif choice == 3:
         x = int(input("Enter ID of employee who needs to be removed: "))
         for f in lst1:
@@ -45,5 +47,6 @@ while 1:
             print("After Change: ")
             for i in lst1:
                 print("ID: ", i.id, "Name: ", i.name)
+
     elif choice == 4:
         exit()
